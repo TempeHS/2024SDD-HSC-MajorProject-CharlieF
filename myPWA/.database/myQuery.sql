96172b6969900701589fb0051edcb7320a9078a0
Charlie <charles.fitzgerald@education.nsw.gov.au>
2024-02-09 12:51:15 +1100

    task/event UI complete
    TODO: display current tasks with their name, date and time below the create task button.

--- myPWA/.database/myQuery.sql
+++ myPWA/.database/myQuery.sql
@@ -1,9 +1,9 @@
 CREATE TABLE tasks (taskID INTEGER NOT NULL PRIMARY KEY, taskName TEXT NOT NULL, taskDateInt INTEGER NOT NULL);
 
-INSERT INTO tasks (taskID, taskName, taskDateInt) VALUES (1,"test","2022024");
+INSERT INTO tasks (taskID, taskName, taskDateInt) VALUES (1,"test","2024-01-01");
 
 -- INSERT INTO tasks (taskID, taskName, taskNumbers) VALUES (45678,"Marty Davis","17");
 
---SELECT * FROM tasks;
+SELECT * FROM tasks;
 
 --SELECT * FROM tasks WHERE name LIKE 'David Smith';
--- myPWA/public/js/calendarScript.js
+++ myPWA/public/js/calendarScript.js
@@ -65,3 +65,65 @@ function dropdownChoice() {
 var intervalId = window.setInterval(function(){
 	console.log("dropdown output = " + taskPriority);
   }, 500);
+
+  function hideShowCreateButton() {
+    var x = document.getElementById("txt2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function saveButton() {
+  var taskNameInput = document.getElementById("taskNameInput").value;
+  var taskNameInputString = taskNameInput;
+  console.log(taskNameInputString);
+  dateAndTimeOutput();
+  var x = document.getElementById("txt2");
+  x.style.display = "none";
+}  
+
+function dateAndTimeOutput() {
+  var dateControl = document.querySelector('input[type="date"]');
+  var timeControl = document.querySelector('input[type="time"]');
+  dateControl.value = dateControl.value;
+  timeControl.value = timeControl.value;
+  dateTimeOutput = timeControl.value + " @ " + dateControl.value;
+  console.log(dateTimeOutput); // prints "2017-06-01" -- change to return later on to create function that grabs this data and puts it into the SQL Database
+  alert("Task/Event has been created, and set to notify you at: " + dateTimeOutput);
+}
+
+let result = ""; 
+fetch('./frontEndData.json') 
+  .then(function (response) { 
+    return response.json(); 
+  }) 
+  .then(function (data) { 
+    appendData(data); 
+  }) 
+  .catch(function (err) { 
+    console.log('error: ' + err); 
+  }); 
+  function appendData(data) { 
+    data.forEach(({ taskID, taskName, taskDateInt } = rows) => { 
+      result += ` 
+       <div class="card"> 
+            <img class="card-image" src="https://windsorpreschool.com/teachers/generic-profile-image-7" alt=""/> 
+            <h1 class="card-name">${taskName}</h1> 
+            <p class="card-about">${taskDateInt}</p> 
+            <a class="card-link" ${taskID}</a> 
+        </div> 
+       `; 
+    }); 
+    document.querySelector(".container").innerHTML = result; 
+  } 
+   
+  if ("serviceWorker" in navigator) { 
+    window.addEventListener("load", function () { 
+      navigator.serviceWorker 
+        .register("/serviceWorker.js") 
+        .then((res) => console.log("service worker registered")) 
+        .catch((err) => console.log("service worker not registered", err)); 
+    }); 
+  } 
