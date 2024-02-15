3d6b73cd6cfc39bc2c03e4a80548b6164de393c1
Charlie <charles.fitzgerald@education.nsw.gov.au>
2024-02-15 11:56:46 +1100

    calender.html improvements
    tasks can now be removed from the page.
    
    TODO: center tasks

--- myPWA/public/js/calendarScript.js
+++ myPWA/public/js/calendarScript.js
@@ -1,5 +1,7 @@
-var taskPriority = 0;
+let taskPriority = 0;
 var taskCount = 0;
+var buttonId = 0;
+var buttonCount = 0;
 
 // allows javascript to wait a set amount of time in ms before executing the next line of code (for debug at the moment)
 function wait(ms){
@@ -53,20 +55,33 @@ window.onclick = function(event) {
   }
 }
 
-function dropdownChoice() {
+function dropdownChoice1() {
   if (document.getElementById("dropdownChoice1").onclick) {
-    taskPriority = 1;   
-  } else if (document.getElementById("dropdownChoice2").onclick) {
-    taskPriority = 2;
-  } else if (document.getElementById("dropdownChoice3").onclick) {
-    taskPriority = 3;
+    taskPriority = 1;
+    console.log("task priority is changed to " + taskPriority)
   }
 }
 
-// debug to see if dropdownChoice function was changing taskPriority
+function dropdownChoice2() {
+  if (document.getElementById("dropdownChoice2").onclick) {
+    taskPriority = 2;
+    console.log("task priority is changed to " + taskPriority)
+  }
+}
+
+function dropdownChoice3() {
+  if (document.getElementById("dropdownChoice3").onclick) {
+    taskPriority = 3;
+    console.log("task priority is changed to " + taskPriority)
+  }
+}
+
+//debug to see if dropdownChoice function was changing taskPriority
+// executes/calls code or functions every x amount of seconds
 /*var intervalId = window.setInterval(function(){
 	console.log("dropdown output = " + taskPriority);
-  }, 500);*/
+  console.log(buttonCount);
+  }, 1000);*/
 
   function hideShowCreateButton() {
     var x = document.getElementById("txt2");
@@ -85,10 +100,12 @@ function saveButton() {
   var x = document.getElementById("txt2");
   x.style.display = "none";
   console.log("task priority is set to: " + taskPriority);
-  saveButtonDataParse(); 
+  saveButtonDataParse();
+  buttonCount = buttonCount +1;
 }  
 
 function saveButtonDataParse () {
+  buttonId = buttonId +1;
   var dateControl = document.querySelector('input[type="date"]');
   var timeControl = document.querySelector('input[type="time"]');
   dateControl.value = dateControl.value;
@@ -102,15 +119,20 @@ function saveButtonDataParse () {
   // and give it some content
   var taskNameInput = document.getElementById("taskNameInput").value;
   var taskNameInputString = taskNameInput;
-  newButton.textContent = "Task Name: " + taskNameInputString + " Date And Time: " + dateTimeOutput + " Task Priority: " + taskPriority;
+  //newButton.textContent = "Task Name: " + taskNameInputString + " Date And Time: " + dateTimeOutput + " Task Priority: " + taskPriority;
+  newButton.textContent = taskNameInputString;
 
   // add the text node to the newly created div
   document.body.appendChild(newButton);
-  document.body.class("dropbtn");
+  newButton.classList.add("dropbtn");
+  newButton.id = "button" + buttonId;
 
   // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementById("div1");
+  let currentDiv = document.getElementById("div");
   document.body.insertBefore(newDiv, newButton, currentDiv);
+  newButton.addEventListener('click', () => {
+    buttonTaskInfo();
+  });
 }
 
 function dateAndTimeOutput() {
@@ -121,4 +143,28 @@ function dateAndTimeOutput() {
   dateTimeOutput = timeControl.value + "-" + dateControl.value;
   console.log(dateTimeOutput); // prints "2017-06-01" -- change to return later on to create function that grabs this data and puts it into the SQL Database
   alert("Task/Event has been created, and set to notify you at: " + dateTimeOutput);
+}
+
+function buttonTaskInfo() {
+  var taskNameInput = document.getElementById("taskNameInput").value;
+  var taskNameInputString = taskNameInput;
+  var dateControl = document.querySelector('input[type="date"]');
+  var timeControl = document.querySelector('input[type="time"]');
+  dateControl.value = dateControl.value;
+  timeControl.value = timeControl.value;
+  dateTimeOutput = timeControl.value + "-" + dateControl.value;
+  alert("Task Name: " + taskNameInputString + " Date And Time: " + dateTimeOutput + " Task Priority: " + taskPriority);
+  deleteTask();
+}
+
+function deleteTask() {
+  window.confirm("Do you want to delete this task?");
+  if (confirm("Task deleted!")) {
+    var elem = document.getElementById("button" + buttonId);
+    elem.parentNode.removeChild(elem);
+    buttonCount = buttonCount -1;
+    buttonId = buttonId -1;
+  } else {
+    alert("Task deletion cancelled!");
+  }
 }
